fa6c43fe29cf9489c9472df69459661b10ca515c
antoniopucciarelli <antonio.pucciarelli@gmail.com>
2022-07-08 10:30:08 +0200

    sending coins between contract in hardhat: done

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,42 +11,50 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 module.exports = {
-    solidity: {
-        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+
+  defaultNetwork: "hardhat",
+
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 2,
+    },
+  },
+
+  gasReporter: {
+    // deployted with test -> yarn hardhat test
+    enabled: false, // if the gas report is not present maybe because it's set to false
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "EUR", // USD
+    token: "BNB", // ETH || BNB || MATIC || AVAX
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
 
-    defaultNetwork: "hardhat",
-
-    networks: {
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 4,
-            blockConfirmations: 2,
-        },
+    user: {
+      default: 1,
     },
 
-    gasReporter: {
-        // deployted with test -> yarn hardhat test
-        enabled: false, // if the gas report is not present maybe because it's set to false
-        coinmarketcap: COINMARKETCAP_API_KEY,
-        outputFile: "gas-report.txt",
-        noColors: true,
-        currency: "EUR", // USD
-        token: "BNB", // ETH || BNB || MATIC || AVAX
+    user1: {
+      default: 2,
     },
 
-    etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+    user2: {
+      default: 3,
     },
-
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        
-        user: {
-            default: 1,
-        },
-    },
+  },
 };
---
+++ scripts/sendCoins.js
@@ -0,0 +1,92 @@
+const emojic = require("emojic");
+const prompt = require("prompt-sync")();
+const { getNamedAccounts, ethers } = require("hardhat");
+
+/**
+ * @notice this function runs a function of the printout solidity script on the blockchain -> make it sure the contract has been deployed on the blockchain
+ */
+async function main() {
+  const funcName = "sendCoins()";
+  const { deployer, user, user1, user2 } = await getNamedAccounts();
+  const [deployerSigner, userSigner, user1Signer, user2Signer] =
+    await ethers.getSigners();
+  const userVec = [deployer, user, user1, user2];
+  const userSignerVec = [deployerSigner, userSigner, user1Signer, user2Signer];
+
+  console.log(
+    "\nRunning " +
+      "\x1b[31m" +
+      funcName +
+      "\x1b[37m \n\n" +
+      emojic.computer +
+      " Deployer address: " +
+      "\x1b[35m" +
+      deployer +
+      "\x1b[37m \n" +
+      "==============================================================="
+  );
+
+  // waiting for the contract to be loaded
+  await ethers.getContract("printout", deployer);
+
+  var inputValue = prompt(
+    "Type amount of coins you want to send to the Smart Contract (ETH): "
+  );
+
+  console.log("Type address name that will send the coin(s): ");
+  for (let ii = 0; ii < userVec.length; ii++) {
+    console.log(
+      "\t" + emojic.computer + " User %d :\x1b[35m %s \x1b[37m",
+      ii,
+      userSignerVec[ii].address
+    );
+  }
+
+  var sendingAddress = prompt("Selected address (number in the list): ");
+  console.log(
+    "\t" + emojic.computer + " User %d :\x1b[35m %s \x1b[37m",
+    sendingAddress,
+    userSignerVec[sendingAddress].address
+  );
+
+  console.log("\nType address name that will receive the coin(s): ");
+  for (let ii = 0; ii < userVec.length; ii++) {
+    console.log(
+      "\t" + emojic.computer + " User %d :\x1b[35m %s \x1b[37m",
+      ii,
+      userVec[ii]
+    );
+  }
+
+  var receivingAddress = prompt("Selected address (number in the list): ");
+  console.log(
+    "\t" + emojic.computer + " User %d :\x1b[35m %s \x1b[37m",
+    receivingAddress,
+    userVec[receivingAddress]
+  );
+
+  // setting up new variable
+  console.log(
+    "\nTransfering coin(s):\n\tfrom: " +
+      emojic.computer +
+      "\x1b[35m %s \x1b[37m \n\tto:   " +
+      emojic.computer +
+      "\x1b[35m %s \x1b[37m",
+    userSignerVec[sendingAddress].address,
+    userVec[receivingAddress]
+  );
+
+  await userSignerVec[sendingAddress].sendTransaction({
+    to: userVec[receivingAddress],
+    value: ethers.utils.parseEther(inputValue),
+  });
+
+  console.log("Coin(s) sent\n");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
